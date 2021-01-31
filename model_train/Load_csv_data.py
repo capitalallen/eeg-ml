0a5bee83b8be10fa66d8d7f0a169f60ec8579d2c
Zeyu Zhang <zzhan785@uwo.ca>
2021-01-31 06:52:20 -0500

    finished

--- model_train/Load_csv_data.py
+++ model_train/Load_csv_data.py
@@ -10,7 +10,9 @@ class Load_data:
         for i in range(len(file_list)):
             ordered_list.append(folder +str(i)+".csv")
         return ordered_list
-
+    def remove_person(self,index,file_list):
+        del file_list[index]
+        return file_list
     def convert_csv_np(self,file_path):
         return np.genfromtxt(file_path, delimiter=',')
 
@@ -20,6 +22,17 @@ class Load_data:
         return data, label
     def get_all_data(self,folder):
         file_list = self.files_in_dir(folder)
+        x,y = [],[]
+        for i in file_list:
+            data = self.convert_csv_np(i)
+            temp_x,temp_y = self.get_data_label(data)
+            x.append(temp_x)
+            y.append(temp_y)
+        return np.asarray(x),np.asarray(y)
+
+    def get_perfect_data(self,folder,index):
+        file_list = self.files_in_dir(folder)
+        file_list = self.remove_person(index,folder)
         x,y = [],[]
         for i in file_list:
             data = self.convert_csv_np(i)
