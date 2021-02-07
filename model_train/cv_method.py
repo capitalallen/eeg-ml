3bb05339c6f8b2cb800f7f01a55fe379089da748
Zeyu Zhang <zzhan785@uwo.ca>
2021-02-07 02:52:57 -0500

    w

---
+++ model_train/cv_method.py
@@ -0,0 +1,71 @@
+import Load_csv_data as lcd 
+import train 
+import numpy as np
+
+def get_x_all(data):
+    x = data[0]
+    for i in range(1,data.size):
+        x = np.concatenate((x,data[i]),axis=0)
+    return x
+
+def get_y_all(data):
+    y = data[0]
+    for i in range(1,data.size):
+        y = np.concatenate((y,data[i]),axis=0)
+    return y  
+
+def get_x_train_test(index,data):
+    train,test = None,None
+    train = data[0] if index !=0 else data[1]
+    for i in range(data.size):
+        if i == index:
+            test = data[i]
+        else:
+            train = np.concatenate((train,data[i]),axis=0)
+    return train,test
+
+def get_y_train_test(index,data):
+    train,test = None,None
+    train = data[0] if index !=0 else data[1]
+    for i in range(data.size):
+        if i == index:
+            test = data[i]
+        else:
+            train = np.concatenate((train,data[i]),axis=0)
+    return train,test
+
+def with_in(folder,type):
+    # load data 
+    get_data = lcd.Load_data() 
+    x,y = get_data.get_all_data(folder)
+    cv_size = x.size
+    # init class
+    train_model = train.Train(folder)
+    # train_model.perform_grid_search(get_x_all(x),get_y_all(y),type)
+    # perform grid search
+    for i in range(cv_size):
+        for j in range(x[i].shape[0]):
+            # 
+        print(x[i].shape)
+    # train_model.perform_grid_search(get_x_all(x),get_y_all(y),type)
+    # for i in range(cv):
+    #     # type,cv_num,x_train,y_train,x_test,y_test
+    #     x_train,x_test = get_x_train_test(i,x)
+    #     y_train,y_test = get_y_train_test(i,y)
+    #     # type,cv_num,x_train,y_train,x_test,y_test
+    #     train_model.ex_train(type,str(i),x_train,y_train,x_test,y_test)
+folder = "../data/"
+exp_type = ['neg_and_net/']
+# for net and neg 
+net_and_neg = ['neg/','net/']
+gender = ["combined"]
+sec = ["_0/"]
+model_type = ['rf']
+folder += "neg_and_net/"
+for n in net_and_neg:
+    for i in gender:
+        for j in sec:
+            for m in model_type:
+                curr_folder = folder + n+i+j
+                # run_train(curr_folder,m)
+                with_in(curr_folder,m)
--- model_train/store_results.py
+++ model_train/store_results.py
@@ -1,8 +1,7 @@
 import pymongo
-
+import numpy as np 
 def store_one_cv(name,cv,coefs,y_train,y_train_pred,y_test,y_test_pred,type="results"):
-    client = pymongo.MongoClient(
-        )
+    client = pymongo.MongoClient("mongodb+srv://zhang:ZHANGzy16!@cluster0.bc8gt.mongodb.net/<dbname>?retryWrites=true&w=majority")
     db = client.get_database('eeg')
     records = db[type]
     id = records.find_one({'name':name})
@@ -23,3 +22,5 @@ def store_one_cv(name,cv,coefs,y_train,y_train_pred,y_test,y_test_pred,type="res
     else:
         id = id.get("_id")
         records.update({"_id":id},{"$set":data})
+# a = np.array([1,2,3])
+# store_one_cv("1","123",a,a,a,a,a,"neg_and_net")
